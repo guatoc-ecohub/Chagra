cf4e1c612855377462f97fe531436894f8f34dbe
Miguel GUerrero <kortux@gmail.com>
2026-08-24 09:42:55 -0500

    index on feat/fermentos-visual: 8bb92a48c feat(fermentos): overhaul visual de FermentosView + entrada visible + fotos CC

--- src/components/AgentScreen/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AgentScreen } from './AgentScreen';
-export { default as ChatHistory } from './ChatHistory';
-export { default as ChatBubble } from './ChatBubble';
-export { default as SuggestedActions } from './SuggestedActions';
