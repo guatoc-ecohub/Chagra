4bbb8f38ab0f8cf822474738dc6805301736b32e
Miguel GUerrero <kortux@gmail.com>
2026-08-24 09:24:51 -0500

    feat(atlas): +6 cultivos staple (frijol/papa/yuca/platano/cafe/cacao) al atlas 3D de semillas

--- src/speciesViewer/atlasData.test.js
+++ src/speciesViewer/atlasData.test.js
@@ -4,7 +4,17 @@ import { ATLAS_STAGES, getAtlasRecord, getMarkers, getStageData } from './atlasD
 describe('atlasData', () => {
   it('expone tres estados y contenido para las especies con lámina curada', () => {
     expect(ATLAS_STAGES.map((stage) => stage.id)).toEqual(['semilla', 'brote', 'planta']);
-    for (const id of ['solanum_lycopersicum', 'zea_mays', 'persea_americana']) {
+    for (const id of [
+      'solanum_lycopersicum',
+      'zea_mays',
+      'persea_americana',
+      'phaseolus_vulgaris',
+      'solanum_tuberosum',
+      'manihot_esculenta',
+      'musa',
+      'coffea_arabica',
+      'theobroma_cacao',
+    ]) {
       const record = getAtlasRecord(id);
       expect(record).not.toBeNull();
       for (const stage of ATLAS_STAGES) {
